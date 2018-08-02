b82b23c1edbc1a31725294d41b47aac83965a200
hase <oldm8reggie@gmail.com>
2018-08-02 16:33:39 +1000

    start tally

---
+++ tally.py
@@ -0,0 +1,14 @@
+
+
+#https://www.reddit.com/r/dailyprogrammer/comments/8jcffg/20180514_challenge_361_easy_tally_program/
+
+def lowerUpper(char):
+    
+    if char.isupper():
+        print("true")
+        return 1
+
+    else:
+        return -1
+
+lowerUpper("T")        
