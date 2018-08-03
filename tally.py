269bae2349a346b797abd87729cb65a18fa84a82
hase <oldm8reggie@gmail.com>
2018-08-03 11:13:10 +1000

    Tally completed and working per specs

--- tally.py
+++ tally.py
@@ -1,14 +1,18 @@
-
-
-#https://www.reddit.com/r/dailyprogrammer/comments/8jcffg/20180514_challenge_361_easy_tally_program/
-
+# https://www.reddit.com/r/dailyprogrammer/comments/8jcffg/20180514_challenge_361_easy_tally_program/
 def lowerUpper(char):
-    
     if char.isupper():
-        print("true")
-        return 1
-
-    else:
         return -1
-
-lowerUpper("T")        
+    else:
+        return 1
+s = 'EbAAdbBEaBaaBBdAccbeebaec'
+team = {}
+# scoring system and commands
+for x in range(len(s)):
+    scores = lowerUpper(s[x])
+    player = s[x].lower()
+    if scores == 1:
+        team[player] = team.get(player, 0) + 1
+    else:
+        team[player] = team.get(player, 0) - 1
+for player, value in sorted(team.items(), key=lambda x: x[1], reverse=True):
+    print(f'{player} : {value}')
